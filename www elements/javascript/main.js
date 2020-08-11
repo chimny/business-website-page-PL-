3dd3ccc4dc6c148592a9490be943d3c62592f53d
chimny <34693368+chimny@users.noreply.github.com>
2020-08-11 22:31:34 +0200

    Add files via upload

--- www elements/javascript/main.js
+++ www elements/javascript/main.js
@@ -13,16 +13,21 @@ burger.addEventListener('click', () => {
 
 
 // typing effect in header section
-const welcomeP = document.querySelector('.welcome p');
-const welcomeH1 = document.querySelector('.welcome h1');
-const welcomeH2 = document.querySelector('.welcome h2');
+const welcomeP = document.querySelector('.welcome p .text');
+const welcomeH1 = document.querySelector('.welcome h1 .text');
+const welcomeH2 = document.querySelector('.welcome h2 .text');
+const spanP = document.querySelector('.welcome p span.cursor');
+const spanH1 = document.querySelector('.welcome h1 span.cursor');
+const spanH2 = document.querySelector('.welcome h2 span.cursor');
+
 
 const welcomePContent = 'witaj!';
 const welcomeH1Content = 'jestem jan nowak';
 const welcomeH2Content = 'web developer - freelancer';
 
-let time = 100;
-let delay = 300;
+const time = 100;
+const flashingCursor = 300;
+const delay = 1000;
 let i = 0;
 
 
@@ -33,35 +38,58 @@ function typingEffect() {
   i++;
   if (i == welcomePContent.length) {
     clearInterval(timer);
-    i = 0;
 
-    function typingEffect2() {
-      welcomeH1.textContent += welcomeH1Content[i];
-      i++;
-      if (i == welcomeH1Content.length) {
-        clearInterval(timer2);
-        i = 0;
+    setTimeout(() => {
+      clearInterval(cursoranimationP);
+      spanP.classList.remove('active');
+      i = 0;
 
-        function typingEffect3() {
-          welcomeH2.textContent += welcomeH2Content[i];
-          i++;
-          if (i == welcomeH2Content.length) {
-            clearInterval(timer3);
-          }
+      function typingEffect2() {
+        welcomeH1.textContent += welcomeH1Content[i];
+        i++;
+        if (i == welcomeH1Content.length) {
+          clearInterval(timer2);
+          setTimeout(() => {
+            clearInterval(cursoranimationH1);
+            spanH1.classList.remove('active');
+            i = 0;
 
+            function typingEffect3() {
+              welcomeH2.textContent += welcomeH2Content[i];
+              i++;
+              if (i == welcomeH2Content.length) {
+
+                clearInterval(timer3);
+              }
+
+            }
+            const cursoranimationH2 = setInterval(() => {
+              spanH2.classList.toggle('active');
+            }, flashingCursor);
+            const timer3 = setInterval(typingEffect3, time);
+          }, delay)
 
         }
-        const timer3 = setInterval(typingEffect3, time);
       }
-    }
 
+      const cursoranimationH1 = setInterval(() => {
+        spanH1.classList.toggle('active');
+      }, flashingCursor);
+      const timer2 = setInterval(typingEffect2, time);
+    }, delay)
 
-    const timer2 = setInterval(typingEffect2, time);
   }
 
 
 
 }
+
+
+
+const cursoranimationP = setInterval(() => {
+  spanP.classList.toggle('active');
+}, flashingCursor);
+
 
 
 
